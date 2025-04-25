6222c3a57ac822ca61def48eeb3bfb57741b5b7f
DhazZzz <110050353+DhazZzz@users.noreply.github.com>
2025-04-25 23:35:19 +0200

    segunda version

--- src/paqPrueba/HolaMundo.java
+++ src/paqPrueba/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Adios Mundo");
+        System.out.println(" Mundo");
     }
     
 }
