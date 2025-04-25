47611acb535b1a489a75a58cb56f4960fa7e5cd6
DhazZzz <110050353+DhazZzz@users.noreply.github.com>
2025-04-25 23:45:35 +0200

    Ultima  version

--- src/paqPrueba/HolaMundo.java
+++ src/paqPrueba/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println(" Mundo");
+        System.out.println("Odio al Mundo");
     }
     
 }
